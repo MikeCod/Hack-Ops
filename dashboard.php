c7d0c107f9168933c71dd5337b88cdc7843fde0a
Mike Cod <mike.cod013@gmail.com>
2019-11-15 18:51:33 +0100

    update

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,7 @@
 <?php
 
 require('config.php');
+session_start();
 
 function button($text, $a, $href = false, $width = 200, $color = "white")
 {
@@ -27,6 +28,11 @@ function button($text, $a, $href = false, $width = 200, $color = "white")
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 	<link rel="stylesheet" media="all" type="text/css" href="include/css/style.css">
 	<link rel="stylesheet" media="all" type="text/css" href="include/css/button.css">
+	<style type="text/css">
+		h1 {
+			padding-left:100px;
+		}
+	</style>
 </head>
 	<body>
 		<div id="banner" style="padding-left:200px; float:left;">
@@ -50,25 +56,48 @@ function button($text, $a, $href = false, $width = 200, $color = "white")
 				echo "</div>";
 			?>
 		</div>
-		<div style="padding-left:250px; width:calc(100% - 250px);">
+		<div class="form-style" style="padding-left:300px; padding-right:50px;width:calc(100% - 350px);">
 			<div id="myprofile" style="display:none;">
-				my profile
+				<h1><?php echo /*$_SESSION['username']*/"Username" ?></h1>
+			</div>
 			<div id="sql-injection" style="display:none;">
-				SQL
+				<h1>SQL Injection</h1>
+				<?php
+					try
+					{
+						$link = new PDO(true)
+					}
+				?>
+				<a href="challenges/SQL-injection/"></a>
 			</div>
 			<div id="csrf" style="display:none;">
 				CSRF
 			</div>
+			<input type="text" id="flag" style="display:none;" placeholder="Flag" onclick="if(windows.event.keyCode == 13) submit_flag();">
 		</div>
 		<script type="text/javascript">
 			var last_page_name = "myprofile";
 			document.getElementById(last_page_name).style.display = "block";
 
+			var type = "";
+
 			function show_page(page_name)
 			{
+				if(page_name == "myprofile")
+					document.getElementById("flag").style.display = "none";
+				else
+				{
+					document.getElementById("flag").style.display = "block";
+					type = page_name;
+				}
 				document.getElementById(last_page_name).style.display = "none";
 				document.getElementById(page_name).style.display = "block";
 				last_page_name = page_name;
+			}
+
+			function submit_flag()
+			{
+
 			}
 		</script>
 		<?php require_once 'footer.php' ?>
