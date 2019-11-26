d5f07dbcfab503f2905546668b9f53d306ff9442
Mike Cod <mike.cod013@gmail.com>
2019-11-26 16:07:26 +0100

    Profile update

--- challenges/sql-injection/2/C_test.php
+++ challenges/sql-injection/2/C_test.php
@@ -1,11 +1,5 @@
 <?php
-/*
-session_start();
-require "../../../config.php";
-require "../../../M_bdd.php";
-require "../../M_init_honeypot.php";
-redirect();
-*/
+
 $link = NULL;
 $dbname = "";
 
@@ -20,21 +14,15 @@ try
 
 	$link->query("CREATE TABLE challenge(flag varchar(64))");
 	$link->query("INSERT INTO challenge(flag) VALUES('".get_flag($type, $difficulty)."')");
-	
-	if(!($response = $link->query("SELECT username, id FROM users WHERE id = ".$_GET['name'])))
+
+	if(!($response = $link->query("SELECT username, id FROM users WHERE id = ".$_GET['id'])))
 		throw new Exception("Request failed");
 
 	if($response->rowCount() != 1)
 		throw new Exception("No user found");
 
 	$user = $response->fetch();
-	$result = "Username: ".$user['username']." ID = ".$user['id']."<br>";
-	/*
-	$_SESSION['result'] = "<script src=\"../../../include/js/sweetalert2.all.js\"></script>\n<script type=\"text/javascript\">\nSwal.fire(
-								\"Congrats !\",
-								\"You can now validate this challenge with the flag ".get_flag($type, $difficulty)."\",
-								\"success\"
-							);</script>";*/
+	$result = "<p>ID = ".$user['id']."<br>Username: ".$user['username']." </p>";
 }
 catch(Exception $e)
 {
