aca9ffc602f74798b3190b62b0acca250c61277a
Mike Cod <mike.cod013@gmail.com>
2020-01-17 14:31:41 +0100

    Plat-In

--- View/dashboard.php
+++ View/dashboard.php
@@ -275,27 +275,6 @@ try
 			{
 				return new Promise(resolve => setTimeout(resolve, ms));
 			}
-
-			function logCookie(cookie) {
-			  if (cookie) {
-			    console.log(cookie.value);
-			  }
-			}
-
-			function getCookie(tabs) {
-			  var getting = browser.cookies.get({
-			    url: tabs[0].url,
-			    name: "favourite-colour"
-			  });
-			  getting.then(logCookie);
-			}
-
-			var getActive = browser.tabs.query({
-			  active: true,
-			  currentWindow: true
-			});
-
-			getActive.then(getCookie);
 		</script>
 		<?php
 
